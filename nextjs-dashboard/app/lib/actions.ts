691c06c0acd99a23f8ed637cab3cd34eb8bbae04
vincent-gaines <vincentanggaines@gmail.com>
2025-08-10 17:29:07 -0400

    chapter 13

---
+++ nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,70 @@
+'use server';
+ 
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import postgres from 'postgres';
+ 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+// Use Zod to update the expected types
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`INSERT INTO invoices (customer_id, amount, status, date)
+   VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
+    
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+/* Similarly to the createInvoice action, here you are:
+
+Extracting the data from formData.
+Validating the types with Zod.
+Converting the amount to cents.
+Passing the variables to your SQL query.
+Calling revalidatePath to clear the client cache and make a new server request.
+Calling redirect to redirect the user to the invoice's page. */
+
+   export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+   
+    const amountInCents = amount * 100;
+   
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+
+   revalidatePath('/dashboard/invoices');
+   redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
+}
